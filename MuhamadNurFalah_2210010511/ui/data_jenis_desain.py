347a358af84528ade115cc4a063ad39ef0974787
Muhamad Nur Falah <106918701+KimoTime@users.noreply.github.com>
2024-11-13 09:54:35 +0700

    Add files via upload

---
+++ MuhamadNurFalah_2210010511/ui/data_jenis_desain.py
@@ -0,0 +1,14 @@
+from PyQt6 import QtWidgets, uic
+import sys
+
+class app5f(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(app5f, self).__init__()
+        uic.loadUi('data_jenis_desain.ui', self)
+
+if __name__ == '__main__':
+    aplikasiku = QtWidgets.QApplication(sys.argv)
+    jendela = app5f()
+    jendela.show()
+
+    sys.exit(aplikasiku.exec())
